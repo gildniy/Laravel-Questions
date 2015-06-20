3526e237cfc526d91c1a9ade356dcf94450bc29e
DraperStudio <DraperStudio@users.noreply.github.com>
2015-06-20 17:01:55 +0200

    Fix questions migration name

--- database/migrations/2015_04_25_000000_create_questions_table.php
+++ database/migrations/2015_04_25_000000_create_questions_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateQuestionsTabler extends Migration
+class CreateQuestionsTable extends Migration
 {
     /**
      * Run the migrations.
